fdeb1f94404ba959fdb3f0904e81efa6a46c1b67
iamkeerthika <keerthikadhamodharan10@gmail.com>
2026-04-03 19:33:11 +0530

    Add files via upload

---
+++ primitive datatypes.java
@@ -0,0 +1,14 @@
+public class hello {
+    public static void main(String args[]) {
+
+        int a = 10;
+        short b = 100;
+        byte c = 45;
+        long d = 120;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
