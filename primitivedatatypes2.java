fa4814ed2513b4bc7f41a8325e0d1e3d21d43b21
iamkeerthika <keerthikadhamodharan10@gmail.com>
2026-04-03 19:33:53 +0530

    Add files via upload

---
+++ primitivedatatypes2.java
@@ -0,0 +1,8 @@
+public class primitivedatatypes2 {
+    public static void main(String args[]){
+        double a=10.235576845345;
+        float b=12.3446f;
+        System.out.println(a);
+        System.out.println(b);
+   }
+}
