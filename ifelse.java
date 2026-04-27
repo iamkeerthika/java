ca93df84bd39b53fd0f9b547d454be3d8698628f
iamkeerthika <keerthikadhamodharan10@gmail.com>
2026-04-27 21:12:56 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,13 @@
+public class ifelse {
+    public static void main(String args[])
+    {
+        int num1= 20;
+        int num2= 30;
+        if(num1==num2){
+            System.out.println("both numbers are equal");
+        }
+        else{
+            System.out.println("not equal");
+        }
+    }
+}
